af79e1afdab99e68f434a201a7d9681b323ceebe
Hugo Martin <hugomartin.menz@gmail.com>
2022-08-26 11:29:09 +0200

    finished practice module 5

--- modulo5/carrito compra laboratorio/js/Readme.md
+++ modulo5/carrito compra laboratorio/js/Readme.md
@@ -14,5 +14,20 @@
 
 >OPCIONAL
 * Si todos los productos son prime mostrar un mensaje "Pedido sin gastos de envío", si no "Este pedido tiene gastos de envío".
+** Ya que todo el contenido de la página está en ingles, he cambiado el mensaje 
 * Mostrar el carrito en un listado de html básico.
 * Aplicar un descuento del 5% si la compra es mayor de 100 €.
+** Se ha usado como precio límite 150€ ya que seleccionando unicamente 1 producto de cada, la suma es de 101,60€.
+
+#### Funciones adicionales.
+* Se ha añadido la posibilidad de hacer click sobre **"+"** o **"-"** pudiendo cambiar el número total de articulos. La cuenta llega hasta 1, ya que si hay productos en el carro de la compra, no podría ser cero. En este caso, se deberían eliminar del carro mediante un boton de eliminar o similar.
+* Al añadir o quitar numero de productos, el precio total del producto se actualiza automáticamente.
+* Se muestra el precio unitario del producto bajo la descripcion del mismo.
+* Muestra debajo del precio unitario, si el producto es premium o no.
+* El subtotal se modifica automáticamente.
+* Muestra si se deben pagar tasas adicionales por envío al no ser todos los productos premium.
+> All products are premium!  Free delivery! ✔️
+> With delivery fees (All products are not Premium) ❌
+* Muestra si se ha aplicado un descuento por ser el subtotal superior a 150€.
+> No discount available (subtotal < 150€) ❌
+> Discount of 5% ✔️
--- modulo5/carrito compra laboratorio/js/app.js
+++ modulo5/carrito compra laboratorio/js/app.js
@@ -1,63 +1,130 @@
-
-function printProduct(product){
-    for (attr in product) console.log(attr.toUpperCase() + ": " + product[attr])
-    return '';
-}
-
-function showShoppingCart(cart){
-    for (product of cart){
-        console.log("-----------");
-        console.log(printProduct(product));
-    };
-}
-
-function deleteProductById(cart, index){
-    for ( product of cart ) if (product.id == index) cart.splice(cart.indexOf(product), 1);
-    return cart;
-}
-
-function getTotalCartValue (cart){
-    var totalValueCart = 0; 
-
-    for (product of cart) totalValueCart += (product.price * product.count);
-    
-    console.log("Total of the Shopping Cart: " + totalValueCart + "€");
-
-    return totalValueCart;
-}
-
-function getPremiumProducts(cart){
-    var premiumProducts = [];
-    for (product of cart){
-        if (product.premium == true){
-            premiumProducts.push(product)
-        }
-    }
-    return premiumProducts;
-}
-
-
-function checkTotalValueDiscount(cart){
-    var discount = 0.05;
-    var totalValue = getTotalCartValue(cart);
-
-    newTotalValue = totalValue > 50 ? ( 1 - discount ) * totalValue : totalValue;
-
-    console.log(totalValue > newTotalValue ? "Discount of " + discount * 100+ "%" : "No discount available")
-    console.log(totalValue > newTotalValue ? "Total with discount: " + newTotalValue.toFixed(2) + "€" 
-    : "Total without discount: " + totalValue + "€")
-
-    return newTotalValue
-}
-
-// functions to print the different exercises
+// functions to print in console the different titles of the exercises
 var printHash = () => console.log("#".repeat(48));
 function printExerciseDescription (text) {
     printHash(); console.log(text); printHash();
 }
 
+//Prints the different products inside the given shopping cart
+function showShoppingCart(cart){
+    for (product of cart){
+        for (attr in product) console.log(attr.toUpperCase() + ": " + product[attr]);
+        console.log("-----------"); 
+    };
+}
 
+// Given a product id and the shopping cart it deletes that product and returns a new cart
+function deleteProductById(cart, index){
+    for ( product of cart ) if (product.id == index) cart.splice(cart.indexOf(product), 1);
+    console.log("Product id to delete: ", index);
+    return cart;
+}
 
+// Geturns the total value of the shopping cart
+// It multiplies the product price and the product count (ammount of the same product)
+function getTotalCartValue (cart){
+    var totalValueCart = 0; 
+    
+    for (product of cart) totalValueCart += (product.price * product.count);
+    
+    console.log("Total of the Shopping Cart: " + totalValueCart + "€");
+    
+    return totalValueCart;
+}
+
+// Returns a new shopping cart with premium products only
+function getPremiumProducts(cart){
+    var premiumProducts = [];
+    
+    for (product of cart) if (product.premium == true) premiumProducts.push(product);
+    
+    return premiumProducts;
+}
+
+// Compares the shopping cart with the cart created with the getPremiumProducts function
+function checkDeliveryFees(cart){
+    // gets new shopping cart with all premium products
+    // if they have the same number of elements, all products are premium
+    premiumProducts = getPremiumProducts(cart);
+    var message = cart.length == premiumProducts.length 
+        ? "All products are premium!  Free delivery! ✔️" 
+        : "With delivery fees (All products are not Premium) ❌";
+    document.getElementById("delivery-fees").innerHTML = message;
+    console.log(message)
+}
+
+// Checks if the total value of the cart is greater than the price limit
+function checkTotalValueDiscount(cart){
+    const PRICE_LIMIT = 150; 
+    const DISCOUNT = 0.05;
+    var totalValue = getTotalCartValue(cart);
+    
+    newTotalValue = totalValue > PRICE_LIMIT ? ( 1 - DISCOUNT ) * totalValue : totalValue;
+    
+    console.log(totalValue > newTotalValue 
+        ? "Discount of " + DISCOUNT * 100 + "%  ✔️" 
+        : "No discount available (subtotal < " + PRICE_LIMIT + "€) ❌");
+    
+    console.log(totalValue > newTotalValue 
+        ? "Total with discount: " + newTotalValue.toFixed(2) + "€" 
+        : "Total without discount: " + totalValue + "€");
+    
+    // Anadido para la parte opcional de mostrar el carrito de la compra en formato HTML
+    document.getElementById("order-discount").innerHTML = (totalValue > newTotalValue 
+        ? "Discount of " + DISCOUNT * 100 + "%  ✔️" 
+        : "No discount available (subtotal < " + PRICE_LIMIT + "€) ❌");
+    
+    return newTotalValue;
+}
+
+// Check if a product is premium and returns "premium product"
+function showPremium(condition){
+    if(condition == true){
+        return "Premium Product";
+    }else{
+        return "";
+    }
+}
+
+// Gets all product details in shopping cart to print them in the HTML page of the cart
+function getShoppingCartDetails(cart){
+    i = 1;
+    for (product of cart){
+        document.getElementById("item-" + i).innerHTML = product.name;
+        document.getElementById("amount-" + i).innerHTML = product.count;
+        document.getElementById("premium-" + i).innerHTML = showPremium(product.premium);
+        document.getElementById("price-unit-" + i).innerHTML = product.price + " €";
+        document.getElementById("price-item-" + i).innerHTML = (product.price * product.count).toFixed(2) + " €";
+        i++;
+    }
+    document.getElementById("subtotal").innerHTML = getTotalCartValue(cart).toFixed(2)+ " €";
+    document.getElementById("total").innerHTML = checkTotalValueDiscount(cart).toFixed(2)+ " €";
+    checkDeliveryFees(shoppingCart);
+}
+
+// Adds or subtracts products from the HTML cart and update the cart
+function handlingCounterButton(event){
+    countButtonClicked = event.path[1].childNodes[3]; // Identifies clicked button HTML element
+    buttonType = event.target.id; // identifies clicked button name
+
+    oldValue = +document.getElementById( countButtonClicked.id ).textContent;
+    
+    if ( buttonType === "button-add" ){
+        document.getElementById( countButtonClicked.id ).innerHTML = oldValue + 1;
+    }else if( oldValue != 1 ){
+        document.getElementById( countButtonClicked.id ).innerHTML = oldValue - 1;
+        oldValue = oldValue;
+    }
+        
+    var idIndexProduct = countButtonClicked.id.split("-")[countButtonClicked.id.split("-").length - 1];
+    shoppingCart[idIndexProduct - 1].count = +document.getElementById( countButtonClicked.id ).textContent;
+    
+    console.log(idIndexProduct);
+    console.log("Shopping cart updated!", shoppingCart)
+    
+    getShoppingCartDetails(shoppingCart);
+}
+
+// Shopping cart arrray of objects
 const shoppingCart = [
     {
     id: 198752,
@@ -89,12 +156,14 @@ const shoppingCart = [
     }
 ];
 
-printExerciseDescription("1. MOSTRAR EL CARRITO DE COMPRA");
+ printExerciseDescription("1. MOSTRAR EL CARRITO DE COMPRA");
 console.log(showShoppingCart(shoppingCart));
 
 printExerciseDescription("2. ELIMINAR UN PRODUCTO DEL CARRITO DE COMPRA");
+var idToDelete = 54657;
 var copyShoppingCart = shoppingCart.slice(0, shoppingCart.length); // creates copy of shoppingCart
-var newShoppingCart = deleteProductById(copyShoppingCart, 54657);
+
+var newShoppingCart = deleteProductById(copyShoppingCart, idToDelete);
 showShoppingCart(newShoppingCart);
 
 // returns the shopping cart value
@@ -107,9 +176,17 @@ showShoppingCart(getPremiumProducts(shoppingCart));
 
 console.log("Opcional: " + "-".repeat(38));
 printExerciseDescription("5. GASTOS DE ENVIO CERO SI TODOS LOS PRODUCTOS SON PREMIUM ");
-premiumProducts = getPremiumProducts(shoppingCart);
-var message = shoppingCart.length == premiumProducts.length ? "Gastos de envio cero" : "Con gastos de envio"
-console.log(message)
+checkDeliveryFees(shoppingCart);
 
-printExerciseDescription("7. APLICAR DESCUENTO SI LA COMPRA ES MAYOR DE 50€ ");
-checkTotalValueDiscount(shoppingCart);
+printExerciseDescription("7. APLICAR DESCUENTO SI LA COMPRA ES MAYOR DE 150€ ");
+checkTotalValueDiscount(shoppingCart); 
+
+// modifies the html with the shopping cart values, description, count and price
+getShoppingCartDetails(shoppingCart);
+
+// If a button of add or subtract is clicked, it updates the shopping cart and the HTML page
+var clickedButton = document.querySelectorAll('button'); // gets all button HTML elements
+for (button of clickedButton) {
+    // button: html element with id button-add or button-minus
+    button.addEventListener('click', handlingCounterButton);
+}
