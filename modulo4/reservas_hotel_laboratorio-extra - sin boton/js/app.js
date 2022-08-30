11df9d53b6a5ffec34a649ca2f57a4dc1e3eb49c
Hugo Martin <hugomartin.menz@gmail.com>
2022-08-30 11:50:04 +0200

    reviewed instructor

--- modulo4/reservas_hotel_laboratorio-extra - sin boton/js/app.js
+++ modulo4/reservas_hotel_laboratorio-extra - sin boton/js/app.js
@@ -1,48 +1,19 @@
-var getNumberNight = () => +document.getElementById( "night-input" ).value;
-var getParkingNumberNight = () => +document.getElementById( 'parking-input' ).value;
 var getTypeRoom = () => document.getElementById( "room-type" ).options[document.getElementById( "room-type" ).selectedIndex].text.toLowerCase().replace(" suite","");
 var getSizeRoom = () => document.getElementById( "room-size" ).options[document.getElementById( "room-size" ).selectedIndex].text.toLowerCase();
-var getSpaStatus = () => +document.getElementById( "option-spa" ).checked;
-var writePriceField = (result) => document.getElementById( "result" ).innerHTML = result;
 
-var cleanNightInputField = () => document.getElementById( 'night-input' ).value = ""; 
-var cleanParkingInputField = () => document.getElementById( 'parking-input' ).value = ""; 
-var handlingWritingNightInput = () => { if ( getNumberNight() == 0 ) cleanNightInputField() };
-var handlingWritingParkingInput = () => { if ( getParkingNumberNight() == 0 ) cleanParkingInputField() };
-
-/* function getBookingPrice( typeRoom, sizeRoom, extraSpaRate, selectedNumberNight, parkingNumberNight ){
-    // spa selected = true
-    var selectedOptionRate =  nightRate [typeRoom] + extraSpaRate;
-    var typeDiscount = 1 + sizeRoomDiscount [sizeRoom];
-    var parkingPriceBooking = parkingNumberNight * parkingNightRate;
-    
-    priceBooking = ( typeDiscount * selectedOptionRate * selectedNumberNight ) + parkingPriceBooking;
-    
-    return priceBooking;
+var firstWritingInputs = (event) => {
+    if ( document.getElementById( event.target.id ).valueAsNumber == 0 ) {
+        document.getElementById( event.target.id ).value = "" ;
+    }
 }
 
 function getResult(){
-    var selectedNumberNight = getNumberNight();
-    var parkingNumberNight = getParkingNumberNight();
+    var selectedNumberNight = document.getElementById( "night-input" ).valueAsNumber;
+    var parkingNumberNight = document.getElementById( 'parking-input' ).valueAsNumber;
+
     var typeRoom = getTypeRoom();
     var sizeRoom = getSizeRoom();
-    var selectedSpa = getSpaStatus();
-    var extraSpaRate = (selectedSpa == 1)  ?  20 : 0;
-
-    finalResult = getBookingPrice( typeRoom, sizeRoom, extraSpaRate, selectedNumberNight, parkingNumberNight );
-    
-    writePriceField( finalResult.toFixed(2) + "€" );
-} */
-
-//// DESPUES DE LA REVISION
-
-
-function getResult(){
-    var selectedNumberNight = getNumberNight();
-    var parkingNumberNight = getParkingNumberNight();
-    var typeRoom = getTypeRoom();
-    var sizeRoom = getSizeRoom();
-    var selectedSpa = getSpaStatus();
+    var selectedSpa =  +document.getElementById( "option-spa" ).checked;
     var extraSpaRate = (selectedSpa == 1)  ?  20 : 0;
 
     var selectedOptionRate =  nightRate [typeRoom] + extraSpaRate;
@@ -51,13 +22,9 @@ function getResult(){
     
     priceBooking = ( typeDiscount * selectedOptionRate * selectedNumberNight ) + parkingPriceBooking;
 
-  /*   finalResult = getBookingPrice( typeRoom, sizeRoom, extraSpaRate, selectedNumberNight, parkingNumberNight ); */
-    writePriceField( priceBooking.toFixed(2) + "€" );
+    document.getElementById( "result" ).innerHTML = priceBooking.toFixed(2) + "€" ;
 }
 
-
-
-///////////////////////////
 // variables
 var nightRate = {
     standard:100,
@@ -72,13 +39,3 @@ var sizeRoomDiscount = {
 };
 
 var parkingNightRate = 10
-
-document.getElementById("night-input").addEventListener("keydown", handlingWritingNightInput);
-document.getElementById("parking-input").addEventListener("keydown", handlingWritingParkingInput);
-
-document.getElementById("room-type").addEventListener("change", getResult);
-document.getElementById("room-size").addEventListener("change", getResult);
-document.getElementById("option-spa").addEventListener("change", getResult);
-document.getElementById("night-input").addEventListener("change", getResult);
-document.getElementById("parking-input").addEventListener("change", getResult);
-
