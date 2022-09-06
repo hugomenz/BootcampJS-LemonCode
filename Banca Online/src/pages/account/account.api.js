e69575710f39814c552f33bac207ee586156a24a
Hugo Martin <hugomartin.menz@gmail.com>
2022-09-06 16:07:22 +0200

    account validations done

---
+++ Banca Online/src/pages/account/account.api.js
@@ -0,0 +1,4 @@
+
+
+// 1. La pagina cuenta bancaria es parecida a la pagina de login.
+// Tenemos dos inputs y un boton.
---
+++ Banca Online/src/pages/account/account.js
@@ -0,0 +1,35 @@
+import { onSubmitForm, onUpdateField, onSetError} from '../../common/helpers/element.helpers';
+import { formValidation } from './account.validations';
+
+let account = {
+    id: '',
+    type: '',
+    alias: '',
+};
+
+onUpdateField('type', event => {
+    const value = event.target.value;
+    account = {
+        ...account,
+        type: value,
+    }
+    formValidation.validateField('type', account.type).then(result => {
+        onSetError('type', result);
+    });
+})
+
+onUpdateField('alias', event => {
+    const value = event.target.value;
+    account = {
+        ...account,
+        alias: value,
+    }
+    formValidation.validateField('alias', account.alias).then(result => {
+        onSetError('alias', result);
+    });
+})
+
+onSubmitForm('save-button', () => {
+    console.log( {account} );
+})
+
