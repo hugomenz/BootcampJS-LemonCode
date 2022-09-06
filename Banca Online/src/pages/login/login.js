50642991eb1837360ae0ab51d0f272794f6bee3c
Hugo Martin <hugomartin.menz@gmail.com>
2022-09-06 14:11:25 +0200

    login api and validators

--- Banca Online/src/pages/login/login.js
+++ Banca Online/src/pages/login/login.js
@@ -1,3 +1,53 @@
+import { onUpdateField, onSubmitForm, onSetError, onSetFormErrors } from '../../common/helpers'
+import { isValidLogin } from './login.api' 
+import { formValidation } from './login.validations' 
 
+let login = {
+    user: '',
+    password: '',
+};
 
-console.log("login page");
+onUpdateField('user', (event) => {
+    const value = event.target.value;
+
+    login = {
+        ...login,
+        user: value 
+    };
+
+    formValidation.validateField('user', login.user).then(result => {
+        onSetError('user', result);
+    });
+});
+
+onUpdateField('password', (event) => {
+    const value = event.target.value;
+    login = {
+        ...login,
+        password: value 
+    };
+
+    
+    formValidation.validateField('user', login.user).then(result => {
+        onSetError('password', result);
+    });
+});
+
+const onNavigate = isValid => {
+    if (isValid){
+        history.pushState(routes.accountList);
+    }else {
+        alert("Usuario y/o contraseña no validos")
+    }
+}
+
+onSubmitForm("login-button", () => {
+    formValidation.validateForm(login).then(result => {
+        onSetFormErrors(result);
+        if (result.succeeded){
+            isValidLogin(login).then(isValid => {
+                console.log({ isValid }); // true / false
+            });
+        }
+    });
+});
