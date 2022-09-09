196e93d13d6d23cff06748d6cf2c11cf0384c2ad
Hugo Martin <hugomartin.menz@gmail.com>
2022-09-09 09:32:00 +0200

    property filter done

--- Portal Inmobiliario/src/pages/property-list/property-list.js
+++ Portal Inmobiliario/src/pages/property-list/property-list.js
@@ -1,20 +1,93 @@
-import { getPropertyList } from './property-list.api'
-import { addPropertyRows } from './property-list.helpers'
-import { mapPropertyListFromApiToVm } from './property-list.mappers'
+import { getPropertyList, getSaleTypeList, getProvinceList } from './property-list.api';
+import { addPropertyRows, setOptions, clearPropertyRows } from './property-list.helpers';
+import { mapPropertyListFromApiToVm, mapFilterToQueryParams } from './property-list.mappers';
+import { roomOptions, bathroomOptions, maxPriceOptions, minPriceOptions} from './property-list.constants';
+import { onUpdateField, onSubmitForm } from '../../common/helpers/element.helpers';
 
-getPropertyList().then(apiPropertyList => {
-    const viewModelPropertyList = mapPropertyListFromApiToVm(apiPropertyList);
-    addPropertyRows(viewModelPropertyList);
+Promise.all(
+    [
+    getPropertyList(),
+    getSaleTypeList(),
+    getProvinceList(),
+    ]
+).then(([ propertyList, saleTypeList, provinceList ]) => {
+    loadPropertyList(propertyList);
+    setOptions(saleTypeList, "select-sale-type", 'Qué venta?');
+    setOptions(provinceList, "select-province", 'Dónde?');
+    setOptions(roomOptions, "select-room", 'Habitaciones?');
+    setOptions(bathroomOptions, "select-bathroom", 'Baños?'); 
+    setOptions(minPriceOptions, "select-min-price", 'Min (EUR)');
+    setOptions(maxPriceOptions, "select-max-price", 'Max (EUR)');
 });
 
+// show the properties
+const loadPropertyList = propertyList => {
+    const viewModelPropertyList = mapPropertyListFromApiToVm(propertyList);
+    addPropertyRows(viewModelPropertyList);
+};
 
-// SEGUIR en 1:22:00
+let filter = {
+    saleTypeId: '',
+    provinceId: '',
+    minRooms: '',
+    minBathRooms: '',
+    minPrice: '',
+    maxPrice: '',
+};
 
+onUpdateField('select-sale-type', event => {
+    const value = event.target.value;
+    filter = {
+        ...filter,
+        saleTypeId: value,
+    };
+});
 
-// Mostrar los datos de las viviendas
-// Para mostrarlos solo necesitamos una imagen
-// titulo de la casa
-// habiutaciones
-// metros cuadrads
-// notas.. (acortarlo tambien)
-// precio viviendas en euros
+onUpdateField('select-province', event => {
+    const value = event.target.value;
+    filter = {
+        ...filter,
+        provinceId: value,
+    };
+});
+
+onUpdateField('select-room', event => {
+    const value = event.target.value;
+    filter = {
+        ...filter,
+        minRooms: value,
+    };
+});
+
+onUpdateField('select-bathroom', event => {
+    const value = event.target.value;
+    filter = {
+        ...filter,
+        minBathRooms: value,
+    };
+});
+
+onUpdateField('select-min-price', event => {
+    const value = event.target.value;
+    filter = {
+        ...filter,
+        minPrice: value,
+    };
+});
+
+onUpdateField('select-max-price', event => {
+    const value = event.target.value;
+    filter = {
+        ...filter,
+        maxPrice: value,
+    };
+});
+
+onSubmitForm('search-button', () => {
+    const queryParams = mapFilterToQueryParams(filter);
+    clearPropertyRows();
+    getPropertyList(queryParams).then(propertyList => {
+        loadPropertyList(propertyList)
+    });
+});
+
--- Portal Inmobiliario/src/pages/property-list/property-list.mappers.js
+++ Portal Inmobiliario/src/pages/property-list/property-list.mappers.js
@@ -17,7 +17,7 @@ const mapPropertyFromApiToVm = property => {
     return {
         id: property.id,
         title: property.title,
-        rooms: `${property.room} ${getRoomWord(property.rooms)}`,
+        rooms: `${property.rooms} ${getRoomWord(property.rooms)}`,
         squareMeter: `${property.squareMeter}m²`,
         notes: `${property.notes.substring(0, 240)}...`,
         price: `${property.price.toLocaleString()}€`,
@@ -28,3 +28,17 @@ const mapPropertyFromApiToVm = property => {
 const getRoomWord = rooms => {
     return rooms > 1 ? 'habitaciones' : 'habitación';
 };
+
+export const mapFilterToQueryParams = filter => {
+    let queryParams = '';
+
+    if (filter.saleTypeId) queryParams = `${queryParams}saleTypeIds_like=${filter.saleTypeId}&`;// like por array
+    if (filter.provinceId) queryParams = `${queryParams}provinceId=${filter.provinceId}&`;  //no like por ser value
+    if (filter.minRooms) queryParams = `${queryParams}rooms_gte=${filter.minRooms}&`;
+    if (filter.minBathRooms) queryParams = `${queryParams}bathrooms_gte=${filter.minBathRooms}&`;
+    if (filter.minPrice) queryParams = `${queryParams}price_gte=${filter.minPrice}&`;
+    if (filter.maxPrice) queryParams = `${queryParams}price_lte=${filter.maxPrice}&`;
+
+    return queryParams.slice(0, -1);
+
+}
